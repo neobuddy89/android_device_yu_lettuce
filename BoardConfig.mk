91866e67e27b4a2247580f9d0fd03981881cc8ea
Zhao Wei Liew <zhaoweiliew@gmail.com>
2016-05-12 18:27:56 +0800

    lettuce: Use the appropriate TaptoWake node
    Change-Id: I136544c9f2f188715b58d5831127bc1da56c3c6f
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -66,7 +66,7 @@ BOARD_SEPOLICY_DIRS += \
     $(DEVICE_PATH)/sepolicy
 
 # Tap to wake
-TARGET_TAP_TO_WAKE_NODE := "/sys/devices/soc.0/78b9000.i2c/i2c-5/5-0040/gesture"
+TARGET_TAP_TO_WAKE_NODE := "/sys/devices/soc.0/78b9000.i2c/i2c-5/5-0040/double_tap_enable"
 
 # Widevine
 BOARD_WIDEVINE_OEMCRYPTO_LEVEL := 3
