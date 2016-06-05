9f4c193a27a0e7ba402261b3c8f6c6f73b1c6436
Pranav Vashi <neobuddy89@gmail.com>
2016-06-05 14:27:00 +0530

    lettuce: Use the appropriate TaptoWake node
    Change-Id: Id7d0997e7bfdfe07c129bc57c6256a95e513b2eb
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- board/hardware.mk
+++ board/hardware.mk
@@ -1,4 +1,4 @@
 # CMHW
 BOARD_HARDWARE_CLASS += $(DEVICE_PATH)/cmhw
 # Tap to wake
-TARGET_TAP_TO_WAKE_NODE := "/sys/devices/soc.0/78b9000.i2c/i2c-5/5-0040/gesture"
+TARGET_TAP_TO_WAKE_NODE := "/sys/devices/soc.0/78b9000.i2c/i2c-5/5-0040/double_tap_enable"
